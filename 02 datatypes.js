504deb421cef70c9679098cc1d42c79dd26fe635
erlove000 <145665604+erlove000@users.noreply.github.com>
2023-09-26 10:31:02 +0000

    updated

---
+++ 02 datatypes.js
@@ -0,0 +1,31 @@
+//"use strict";// treat all code newer version
+
+
+//alert("3+3")// we are using nodejs not browsing
+
+
+//console.log(3+3);console.log(); /// codwe readbility should be high( code readable plz easy to read)
+
+
+
+//let name="rahul"
+//let age= 20
+//let isLoggedIn=false
+
+//number=>2 to power 53
+//bigInt
+// string=>""
+//boolean=>true/false
+//null=> standalone value
+//undefined=>
+//symbol=>unique
+//object
+
+console.log(typeof "rahul")
+console.log(typeof age)
+console.log(null)//object
+console.log(undefined)//undefined
+
+
+
+

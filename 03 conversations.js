f3255634b7ae4df2fd8a86f2f4f31bf8e37ba30f
erlove000 <145665604+erlove000@users.noreply.github.com>
2023-09-26 10:32:01 +0000

    updated

---
+++ 03 conversations.js
@@ -0,0 +1,41 @@
+// let score=33//
+
+
+// console.log(typeof score)
+
+// output is==== number
+
+
+// another method
+
+
+let score="33"/// when let score 33abc than output is===Nan
+
+// console.log(typeof (score));/// output is string
+
+let valueInNumber=Number(score)////Number is always is capital bcoz class based
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=> NaN
+// true=> 1
+// false=>0
+
+
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// output is =>true 
+
+// let isLoggedIn="rahul"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// 1=> true; 0=> false
+// "" => false
+
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

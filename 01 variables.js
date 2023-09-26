e675ce55d87291664917a97f428a27718a6486f4
erlove000 <145665604+erlove000@users.noreply.github.com>
2023-09-26 10:28:46 +0000

    updated

---
+++ 01 variables.js
@@ -0,0 +1,14 @@
+const accountId=14453
+let accountEmail="love@gmail.com"
+var accountPassword="12345"
+accountCity="Mumbai"
+
+
+let accountState;
+// prefer not use var //
+
+accountEmail="hdfc@gmail.com"
+accountPassword="654321"
+accountCity="Jalandhar"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])

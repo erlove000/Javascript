7140dfbc227d5bfbfa62dc4a136c229c74019476
erlove000 <145665604+erlove000@users.noreply.github.com>
2023-09-26 10:33:47 +0000

    updated

---
+++ practice.js
@@ -0,0 +1,16 @@
+const elementId=123
+let accountName="Ram"
+let accountCity="mumbai"
+
+//console.log(elementId,accountName,accountCity)//
+//console.table([elementId,accountName,accountCity]);//
+
+let accountAddress;
+let accountNumber;
+console.table([elementId,accountName,accountCity,accountAddress,accountNumber]);
+let a=("1"+2+2)
+console.log(typeof String);
+console.log(a);
+
+
+

03fbf3d26b93b03bdef4cf3c76198ea86ecc7372
erlove000 <145665604+erlove000@users.noreply.github.com>
2023-09-27 04:27:09 +0000

    updated

---
+++ basics/03.1 operations.js
@@ -0,0 +1,35 @@
+// //2+2//2-2
+
+// let value=3
+// let negValue= -value
+// console.log(negValue);//
+// output is =>-3
+
+
+let str1="hello"
+let str2=" rahul"
+
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1+"2")
+// console.log("1"+2+"2")
+// console.log(1+2+"2")
+
+//console.log(+true) //output =>1
+//console.log(+""); //output =>0///
+///////////
+
+
+
+let num1, num2, num3
+num1= num2=num3=2+2
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
+
+
+

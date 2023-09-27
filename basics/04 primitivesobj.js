9e494082e0dc17528b8449b1d64ebaee19bfd0f1
erlove000 <145665604+erlove000@users.noreply.github.com>
2023-09-27 04:40:14 +0000

    updated

--- basics/04 primitivesobj.js
+++ basics/04 primitivesobj.js
@@ -6,4 +6,16 @@ let d = BigInt("567")  //+ BigInt("3")/// output is 570;
 let e = "harry"
 let f = Symbol("i am a nice symbol")
 let g = undefined
-console.log(a,b,c,d,e,f,g);
+// console.log(a,b,c,d,e,f,g); output is
+
+// console.log(typeof d);
+
+///object in js//
+
+const item={
+    "rahul" : true,
+    "arun" : false,
+    "lovish":567,
+    "rohan": undefined
+}
+console.log(item["arun"]);

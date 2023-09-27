cb408931280e5b55dd039542c2ba5c3e323f178d
erlove000 <145665604+erlove000@users.noreply.github.com>
2023-09-27 05:13:32 +0000

    updated

---
+++ basics/05 ch1practiceset.js
@@ -0,0 +1,18 @@
+//chapter 1 Questin1//
+let a = "rahul";
+let b = 5
+console.log(a+b);
+
+//Question 2//
+console.log(typeof (a+b));
+
+const a1 = {
+    name:"harsh",
+    Section : 1,
+    isPrincipal: false,
+}
+a1['friend']= "captain"; 
+console.log(a1);
+
+
+

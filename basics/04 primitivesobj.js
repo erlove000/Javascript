929e7c7ddded1a2da9be1a7507d837ec9b83eaf0
erlove000 <145665604+erlove000@users.noreply.github.com>
2023-09-27 04:26:23 +0000

    primitives and object

---
+++ basics/04 primitivesobj.js
@@ -0,0 +1,9 @@
+//bb nn ss u// boolean,big int,null,number,string,symbol,undefined//
+let a = null;
+let b = 345;//numner
+let c = true;
+let d = BigInt("567")  //+ BigInt("3")/// output is 570;
+let e = "harry"
+let f = Symbol("i am a nice symbol")
+let g = undefined
+console.log(a,b,c,d,e,f,g);

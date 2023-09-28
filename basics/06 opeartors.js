16be4fc039572a6b8977e81e9956a7221fe93c16
erlove000 <145665604+erlove000@users.noreply.github.com>
2023-09-28 04:38:45 +0000

    updated

---
+++ basics/06 opeartors.js
@@ -0,0 +1,14 @@
+// 77;
+// "love"
+
+let a =45;
+let b =4;
+console.log("a+b =",a+b);
+console.log("a-b =",a-b);
+console.log("a/b =",a/b);
+console.log("a**b =",a**b);
+
+let c =10;
+let d =4;
+ console.log("d++ =",d++);
+ console.log("++d =",++d);

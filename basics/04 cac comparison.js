a990ae236f3c5596f72c5d71cc92d589969000bd
erlove000 <145665604+erlove000@users.noreply.github.com>
2023-09-29 05:15:55 +0000

    updated

---
+++ basics/04 cac comparison.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+
+
+// console.log("2">1); output=>true
+// console.log("02">1); output=>true
+
+// console.log(null>0);//false
+// console.log(null==0);// value false
+// console.log(null>=0);//value  true
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+/// all three undefined are false
